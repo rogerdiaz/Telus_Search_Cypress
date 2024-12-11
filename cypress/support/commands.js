0414105f5514a54c388b2c53a09d881bd9b9cb64
Roger Artemio Diaz Fuentes <dia12176@gmail.com>
2024-12-11 15:41:28 -0600

    Add region selector method command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import search from "../integration/PageObjects/search";
+const superNav = new search();
+
+Cypress.Commands.add("regionSelector", (region, language, confirmationFr) => {
+  superNav.getRegionToggler().click();
+  superNav
+    .getCurrentRegion()
+    .invoke("attr", "aria-label")
+    .then(function (value) {
+      let togglerRegion = value;
+      let re = "";
+      cy.log(language);
+      if (language == "EN") {
+        re = "\n              Your current region is set to,\n              ";
+      } else if (language == "FR") {
+        re = "\n              Votre région est réglée à,\n              ";
+      }
+      togglerRegion = togglerRegion.replace(re, "");
+      re = "\n            ";
+      togglerRegion = togglerRegion.replace(re, "");
+      if (togglerRegion != region) {
+        superNav.getRegionSelector().contains(region).click({ force: true });
+        superNav
+          .getCurrentRegion()
+          .invoke("attr", "aria-label")
+          .then(function (value) {
+            let togglerRegion = value;
+            let re = "";
+            if (language == "EN") {
+              re =
+                "\n              Your current region is set to,\n              ";
+            } else if (language == "FR") {
+              re = "\n              Votre région est réglée à,\n              ";
+            }
+            togglerRegion = togglerRegion.replace(re, "");
+            re = "\n            ";
+            togglerRegion = togglerRegion.replace(re, "");
+            cy.log("region" + togglerRegion);
+            cy.log("region" + region);
+            if (language == "EN") {
+              expect(togglerRegion.includes(region)).to.be.true;
+            } else if (language == "FR") {
+              expect(togglerRegion.includes(confirmationFr)).to.be.true;
+            }
+          });
+      }
+    });
+});
